5a532e507135efc09dd0edddf627bc8798379075
Tobias Böker <tb@softwarepioniere.de>
2018-12-19 14:14:24 +0100

    azure ad updates

--- src/SoftwarePioniere.AspNetCore/AzureAdExtensions.cs
+++ src/SoftwarePioniere.AspNetCore/AzureAdExtensions.cs
@@ -2,8 +2,13 @@
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 // ReSharper disable CheckNamespace
 // ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
@@ -26,9 +31,8 @@ namespace SoftwarePioniere.AspNetCore
 
     public static class AzureAdAuthenticationBuilderExtensions
     {
-        public static AuthenticationBuilder AddAzureAd(this AuthenticationBuilder builder) => builder.AddAzureAd(_ => { });
-
-        public static AuthenticationBuilder AddAzureAd(this AuthenticationBuilder builder, Action<AzureAdOptions> configureOptions)
+     
+        public static AuthenticationBuilder AddAzureAd(this AuthenticationBuilder builder, Action<AzureAdOptions> configureOptions, string[] contextTokenAddPaths = null)
         {
             Console.WriteLine("AddAzureAd");
 
@@ -59,7 +63,59 @@ namespace SoftwarePioniere.AspNetCore
                 options.ClaimsIssuer = settings.IssuerUrl;
                 options.RequireHttpsMetadata = false;
                 options.SaveToken = true;
-                options.TokenValidationParameters = tokenValParam;
+                options.TokenValidationParameters = tokenValParam;  
+                
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        if (context.SecurityToken is JwtSecurityToken token)
+                        {
+                            if (context.Principal.Identity is ClaimsIdentity identity)
+                            {
+                                identity.AddClaim(new Claim("access_token", token.RawData));
+                                identity.AddClaim(new Claim("tenant", settings.TenantId));
+                                identity.AddClaim(new Claim("provider", "aad"));
+                            }
+                        }
+
+                        return Task.CompletedTask;
+                    },
+
+                    OnMessageReceived = context =>
+                    {
+                        //https://docs.microsoft.com/de-de/aspnet/core/signalr/authn-and-authz?view=aspnetcore-2.1
+                        if (contextTokenAddPaths != null)
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+
+                            bool matchesAny = false;
+
+                            if (!string.IsNullOrEmpty(accessToken))
+                            {
+                                // If the request is for our hub...
+                                var path = context.HttpContext.Request.Path;
+
+                                foreach (var s in contextTokenAddPaths)
+                                {
+                                    if (path.StartsWithSegments(s))
+                                    {
+                                        matchesAny = true;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if (matchesAny)
+                            {
+                                // Read the token out of the query string
+                                context.Token = accessToken;
+                            }
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
             });
 
             Console.WriteLine("AddAzureAd: Adding AddAuthorization Admin Policy");
